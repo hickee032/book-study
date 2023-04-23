c91656c2483ca1dab4beda1f34447e978db5e772
Hyun <hickee32@gmail.com>
2023-04-23 20:46:43 +0900

    sec02

---
+++ Do_it/sec02/sec_02_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02 {
+    class Program {
+        public static void Main(string[] args) {
+            bool boolvariable = true;
+            int intvariable = 10;
+            float floatvariable = 3.4f;
+            char charvariable = 'a';
+
+            Console.WriteLine(boolvariable);
+            Console.WriteLine(intvariable);
+            Console.WriteLine(floatvariable);
+            Console.WriteLine(charvariable);
+        }
+    }
+}
