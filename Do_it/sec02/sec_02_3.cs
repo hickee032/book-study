8f0c60857fd58efc4c6091c552ddf17ffa4d703f
Hyun <hickee32@gmail.com>
2023-04-24 23:24:36 +0900

    sec_02

---
+++ Do_it/sec02/sec_02_3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02_2 {
+    internal class Program {
+        public static void Main(string[] args) {
+
+
+                for (int i = 0; i < 11; i++) {
+                    Console.WriteLine(i);
+                }
+
+                for (int i = 10; i >= 0; i--) {
+                    Console.WriteLine(i);
+                }
+
+            int i = 1;
+            while (i<110) {
+                Console.WriteLine(i);
+                i++;
+                if (i == 100) {
+                    Console.WriteLine("100  돌파");
+                }
+            }
+        }
+    }
+}
