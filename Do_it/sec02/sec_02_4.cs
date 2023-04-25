b0c78d55596cd0753ff4c00a18f2d452596f3842
Hyun <hickee32@gmail.com>
2023-04-25 23:25:39 +0900

    sec_02

---
+++ Do_it/sec02/sec_02_4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02_3 {
+    internal class Program {
+        public static void Main(string[] args) {
+
+            int result, num1, num2;
+            result= 3+1;
+            Console.WriteLine(result);
+
+            result= 3-1;
+            Console.WriteLine(result);
+
+            num1= 2;
+            result= 5*num1;
+            Console.WriteLine(result);
+
+            num2= 10;
+            result = num2 / 3;
+            Console.WriteLine(result);
+
+            result = 10 % 2;
+            Console.WriteLine(result);
+        }
+    }
+}
