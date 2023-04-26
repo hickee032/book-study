25cf143225111fcca03c10b00b1c3ff2ad5d52ac
Hyun <hickee32@gmail.com>
2023-04-26 23:23:53 +0900

    sec_02

---
+++ Do_it/sec02/sec_02_5/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02_4 {
+    class Person {
+
+        public string Name;
+        public string Birthday;
+        public string Gender;
+
+        public void Eat() {
+            Console.WriteLine(Name + " 밥을 먹습니다.");
+        }
+        public void Walk() {
+            Console.WriteLine(Name + " 걷습니다");
+        }
+        public void  Run() {
+            Console.WriteLine(Name + " 뜁니다");
+        }
+
+
+    }
+}
---
+++ Do_it/sec02/sec_02_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sec_02_4 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Person p1;
+            p1= new Person();
+            p1.Name = "철수";
+            p1.Eat();
+
+        }
+    }
+}
